c7cf37a4809880ee110d3c3a29f05b6b415c7305
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-11-25 15:06:46 +0700

    Add files via upload

---
+++ shop/jastip/config.js
@@ -0,0 +1,12 @@
+{
+  "name": "jastip",
+  "description": "jastip",
+  "logo": "",
+  "sheetUrl": "https://docs.google.com/spreadsheets/d/1vQq9oCjGe9CcY7mURLuDO8JoKUChNr1Xm9uuJbWgAJM/export?format=csv",
+  "theme": {
+    "accent": "#2f8f4a",
+    "bg": "#f7f9f8",
+    "card": "#ffffff"
+  },
+  "wa": "6289668081647"
+}
