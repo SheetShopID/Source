d6152f4af4613ad335c53ecc006b4b403669792f
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-11-26 16:52:05 +0700

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Selamat datang di -----</h1>
-      <p>Platform pembuatan toko otomatis dengan Google Sheet.</p>
-      <p>Gunakan subdomain: https://NAMA.tokoinstan.online</p>
-    </div>
-  );
-}
