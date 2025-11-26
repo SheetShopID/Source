00da2606a3c239f3ffc7c65669ff7b4047d4cb59
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-11-26 09:35:01 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-export default function Home(){ return <div>Sheetshop Ready</div>; }
+export default function Home() {
+  return (
+    <div style={{padding: 40}}>
+      <h1>Selamat datang di TokoInstan.online</h1>
+      <p>Platform pembuatan toko otomatis dengan Google Sheet.</p>
+    </div>
+  );
+}
