ac003a63a1140ceafff4c1545e16f8feb09b2fb0
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-11-26 09:37:36 +0700

    Update page.js

--- app/[shop]/page.js
+++ app/[shop]/page.js
@@ -1,12 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import { ref, get } from "firebase/database";
 
-export default async function Shop({ params }) {
-  const snap = await get(ref(db, "shops/" + params.shop));
-  const data = snap.val();
-  if (!data) return <div>Toko tidak ditemukan</div>;
+export default function Shop({ params }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchShop() {
+      const snap = await get(ref(db, "shops/" + params.shop));
+      setData(snap.val());
+    }
+    fetchShop();
+  }, [params.shop]);
+
+  if (!data) return <div style={{padding:20}}>Loading toko...</div>;
+
   return (
-    <div>
+    <div style={{padding:20}}>
       <h1>{data.name}</h1>
       <p>{data.desc}</p>
       <p>WA: {data.wa}</p>
