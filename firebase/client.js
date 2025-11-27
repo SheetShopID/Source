308906d59424d952d59ec34603b6a91971779ca6
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-11-27 11:24:10 +0700

    Update client.js

--- firebase/client.js
+++ firebase/client.js
@@ -2,14 +2,15 @@ import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
+    apiKey: "AIzaSyBSJ-CWTC9TjSGL9b5hiSWRmttH-F-M2bM",
+    authDomain: "ordersheetshopid.firebaseapp.com",
+    databaseURL: "https://ordersheetshopid-default-rtdb.firebaseio.com",
+    projectId: "ordersheetshopid",
+    storageBucket: "ordersheetshopid.firebasestorage.app",
+    messagingSenderId: "894838941192",
+    appId: "1:894838941192:web:19008782f9bf5df25e13bd",
+    measurementId: "G-5JT743CJ1V"
+  };
 
 // Cegah double initialization
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
