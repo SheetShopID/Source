e67ae880042b4430640657a819b3af97cb19b08c
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-12-24 14:56:14 +0700

    Update route.js

--- app/api/get-shop/route.js
+++ app/api/get-shop/route.js
@@ -7,28 +7,19 @@ export async function GET(req) {
     const shop = searchParams.get("shop");
 
     if (!shop) {
-      return NextResponse.json(
-        { error: "Shop param wajib" },
-        { status: 400 }
-      );
+      return NextResponse.json({ error: "Shop param required" }, { status: 400 });
     }
 
     const snapshot = await db.ref(`shops/${shop}`).get();
 
     if (!snapshot.exists()) {
-      return NextResponse.json(
-        { error: "Toko tidak ditemukan" },
-        { status: 404 }
-      );
+      return NextResponse.json({ error: "Shop not found" }, { status: 404 });
     }
 
     return NextResponse.json(snapshot.val());
+
   } catch (err) {
     console.error("GET SHOP ERROR:", err);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
-/*tes*/
