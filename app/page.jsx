2a45d22dddeb5e58eee2ea04fbbf1fcaa401766d
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2025-12-24 14:37:53 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,5 @@
-import { headers } from "next/headers";
-import ShopPage from "../components/ShopPage";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const shop = headers().get("x-shop-id");
-
-  if (!shop) {
-    return (
-      <div style={{ padding: 20 }}>
-        <h1>Selamat datang di TokoInstan</h1>
-        <p>Gunakan subdomain seperti jastip.tokoinstan.online</p>
-      </div>
-    );
-  }
-
-  return <ShopPage shop={shop} />;
+  redirect("/register");
 }
