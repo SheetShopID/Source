b2f34d307e254eb81695f4b4d9cd64fa7b146553
SheetShopID <5p7v2j54ck@privaterelay.appleid.com>
2026-01-18 21:42:27 +0700

    Delete components directory

--- components/themes/_fallback.jsx
+++
@@ -1,8 +0,0 @@
-export default function FallbackTheme() {
-  return (
-    <div style={{ padding: 20, textAlign: "center" }}>
-      <h2>⚠️ Theme not found</h2>
-      <p>This theme does not exist. Using fallback.</p>
-    </div>
-  );
-}
--- components/themes/theme-map.js
+++
@@ -1,5 +0,0 @@
-export const THEME_MAP = {
-  food: () => import("./food"),
-  jasa: () => import("./jasa"),
-  katalog: () => import("./katalog"),
-};
